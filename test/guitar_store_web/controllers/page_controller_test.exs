6a59abf77a6c0fa6fc43177397811e462a532bec
Carlos Souza <carloshrsouza@gmail.com>
2021-06-23 18:40:51 -0400

    Fix test and add Elixir version

--- test/guitar_store_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GuitarStoreWeb.PageControllerTest do
-  use GuitarStoreWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
